c624005b12fe0f12a5e6fef78321568203a6b559
OmelchenkoGleb <glebomelchenko911@gmail.com>
2023-02-04 20:28:26 +0200

    121

---
+++ src/main/java/diplom/try1/Controller/DExel.java
@@ -0,0 +1,32 @@
+package diplom.try1.Controller;
+
+import ch.qos.logback.core.model.Model;
+import diplom.try1.DAO.ExelParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class DExel {
+
+    @Autowired
+    ExelParser exelParser;
+
+    @GetMapping("/")
+    public String start(Model model){
+
+
+        return "/index";
+    }
+
+    @PostMapping("/load")
+    public String load(Model model, @RequestParam MultipartFile file){
+        System.out.println(file.getOriginalFilename());
+        exelParser.parser(file);
+
+        return "/index";
+    }
+}
---
+++ src/main/java/diplom/try1/DAO/ExelParser.java
@@ -0,0 +1,12 @@
+package diplom.try1.DAO;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class ExelParser {
+
+    public void parser(MultipartFile multipartFile){
+
+    }
+}
